9ca4ad1f607aa2a4b70bb504a1aebdd31b479c5e
Bagus Rianto <bagus@outlook.com>
2018-01-06 22:34:40 +0700

    upgrade next and reactjs

--- pages/_document.js
+++ pages/_document.js
@@ -1,7 +1,7 @@
-import Document, { Head, Main, NextScript } from "next/document";
-import { ServerStyleSheet, injectGlobal } from "styled-components";
+import Document, { Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet, injectGlobal } from 'styled-components';
 
-import styledNormalize from "styled-normalize";
+import styledNormalize from 'styled-normalize';
 
 injectGlobal`
   ${styledNormalize}
@@ -54,21 +54,30 @@ export default class MyDocument extends Document {
                 async: true
               },
               h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/wf-loading/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
-            })(document);`
+            })(document);`,
+            }}
+          />
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=UA-112006155-1"
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){
+              dataLayer.push(arguments);
+            }
+            gtag('js', new Date());
+
+            gtag('config', 'UA-112006155-1');
+            `,
             }}
           />
         </Head>
         <body>
           <Main />
           <NextScript />
-          <script async src="https://www.googletagmanager.com/gtag/js?id=UA-112006155-1"></script>
-          <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', 'UA-112006155-1');
-          </script>
         </body>
       </html>
     );
