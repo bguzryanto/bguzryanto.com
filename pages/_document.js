dc57df6782c4f4fb6855ceae57e87b2ab2c0839d
Bagus Rianto <bagus@outlook.com>
2018-01-06 22:34:12 +0700

    add ga script

--- pages/_document.js
+++ pages/_document.js
@@ -61,6 +61,14 @@ export default class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          <script async src="https://www.googletagmanager.com/gtag/js?id=UA-112006155-1"></script>
+          <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'UA-112006155-1');
+          </script>
         </body>
       </html>
     );
