d1e6847bb681b9979c322eda0cb83031b13581df
Bagus Rianto <bagus.rianto@grab.com>
2019-08-09 02:54:05 +0700

    feat: add mobile support

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,42 @@
 import React from "react";
 import Head from "next/head";
 import styled from "styled-components";
+import breakpoint from 'styled-components-breakpoint';
+
 import { Flex, Box } from "grid-styled";
 
 const StyledAboutMe = styled.div`
-  font-size: 2rem;
-  line-height: 2.5rem;
-  margin-top: 7.5rem;
-  margin-left: 7.5rem;
+
   max-width: 70rem;
   text-align: justify;
   font-family: "Roboto Mono", monospace;
+
+  ${breakpoint('mobile')`
+    font-size: 16px;
+    margin: 2.5rem 2rem 0 2rem;
+    font-size: 1.3rem;
+    line-height: 2.1rem;
+  `};
+
+  ${breakpoint('desktop')`
+    font-size: 16px;
+    font-size: 2rem;
+    line-height: 2.5rem;
+    margin: 7.5rem 0 0 7.5rem;
+  `};
+
 
   p {
     font-size: 1.6rem;
     line-height: 2.9rem;
     color: #444;
     text-align: justify;
+  }
+
+  ul {
+    li {
+      font-weight: normal;
+    }
   }
 
   a {
@@ -46,8 +66,7 @@ export default () => (
             I am software engineer at Grab based in 🇸🇬Singapore. I working on
             things that interest me the most, currently it's crossing across
             frontend development, high scale-distributed system, and cloud
-            infrastructure. To make my brain busy I keep in touch with machine
-            learning field, linux development and play some MMORPG game.
+            infrastructure.
           </p>
           <p>
             My daily activities consist of:
@@ -58,14 +77,15 @@ export default () => (
                 Maintain infrastructure, systems operations and reliability
               </li>
               <li>
-                Adhoc things that require skills on cli, code
-                navigation, ripgrep, etc
+                Adhoc things that require skills on cli, code navigation,
+                ripgrep, etc
               </li>
             </ul>
           </p>
           <p>
-            There are more things I want to say, how about follow me + send me a
-            tweet at{" "}
+            To make my brain busy I keep in touch with machine learning field,
+            linux development and play some MMORPG game. There are more things I
+            want to say, how about follow me + send me a tweet at{" "}
             <a href="http://twitter.com/bguzryanto" target="_blank">
               @bguzryanto
             </a>
